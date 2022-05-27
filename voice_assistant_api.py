b563216035f383364d7c323c178c9f4fa018f2d2
Nahuel Passano <passano43632@estudiantes.untref.edu.ar>
2022-05-27 16:23:00 -0300

    feat: Update voice_assistant_api for speech to text with w2v2 inference

--- voice_assistant_api.py
+++ voice_assistant_api.py
@@ -1,42 +1,54 @@
-from fastapi import FastAPI, Path, Request
-from typing import Optional
-import requests, json, os
+import os
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from speech_to_text import speech_to_text_wav2vec2 as stt_w2v2
+import sounddevice as sd
 
-
+    # Load weather api key
 load_dotenv('keys/weather_api_key.env')
 api_key = os.getenv("OPENWEATHERMAP_KEY")
 
+    # Load model of speech to text
+model, processor = stt_w2v2.load_model()
+
+    # Deploying FastAPI
 app = FastAPI()
 
+class Audio(BaseModel):
+    audio_path: str
 
-@app.get('/')
-def home():
-    return {'Data': 'Test'}
+@app.get('/get-response-from-audio/')
+async def get_response_from_audio(audio_path: Audio):
+    
+    audio_array = stt_w2v2.audio_to_array(audio_path.audio_path)
 
-@app.get('/get-response-from-audio')
-def get_response_from_audio(audio_path: str):
+    predicted_sentence = stt_w2v2.inference_model(model, processor, audio_array)
+    
+    output = jsonable_encoder({'Prediction':predicted_sentence})
+    # output = jsonable_encoder(audio_path)
+    """
     ### load audio
     # print(audio_path)
     ### STT
     ### NLP
     ### Information query
-    print(audio_path)
-    # direccion web desde donde solicitaremos la informacion
-    try:
-        base_url = "http://api.openweathermap.org/data/2.5/weather?"
-        # ciudad (se lo mas especifico posible en el nombre)
-        city_name = "Ituzaingo"
-        # esta es la URL completa con la informacion concatenada para realizar la petición correcta
-        complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric"	
-        # Ejecutamos la consulta
-        response = requests.get(complete_url)
-        # Obtenemos la respuesta en formato JSON
-        info = response.json()["main"]
-    except:
-        info = 'que decis flaco'
-    return {'Response': info, 'Audio path': audio_path}
+    # print(audio_path{'audio_path'})
+    # # direccion web desde donde solicitaremos la informacion
+    # try:
+    #     base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    #     #     city_name = "Ituzaingo"
+    #     # esta es la URL completa con la informacion concatenada para realizar la petición correcta
+    #     complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric"	
+    #     response = requests.get(complete_url)
+    #     # Obtenemos la respuesta en formato JSON
+    #     info = response.json()["main"]
+    # except:
+    #     info = 'que decis flaco'
+    """
+    return JSONResponse(output)
 
 if __name__ == '__main__':
     import os
