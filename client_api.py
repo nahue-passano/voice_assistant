e75ea60724e78a1bf90bf67eb288deb3898a2aee
Nahuel Passano <passano43632@estudiantes.untref.edu.ar>
2022-05-27 16:22:12 -0300

    feat: Update client API

--- client_api.py
+++ client_api.py
@@ -2,7 +2,10 @@ import json
 import requests
 
 url = 'http://127.0.0.1:8000'
-endpoint = '/get-response-from-audio'
+endpoint = '/get-response-from-audio/'
 path = 'speech_to_text/test_voice.wav'
-response = requests.get(url+endpoint+'?audio_path='+path)
-print(response.json)
+
+data_sent = json.dumps({'audio_path': path})
+
+response = requests.get(url+endpoint, data = data_sent)
+print(response.json())
