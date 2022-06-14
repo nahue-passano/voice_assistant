4ff16065a5a8a2851e46091c0cf649b797b5d0ca
Nahuel Passano <passano43632@estudiantes.untref.edu.ar>
2022-06-14 01:47:06 -0300

    feat: Update voice-assistant and Dockerfile ok

--- voice_assistant/voice_assistant_api.py
+++ voice_assistant/voice_assistant_api.py
@@ -2,19 +2,13 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-from dotenv import load_dotenv
 from pydantic import BaseModel
 import speech_to_text_wav2vec2 as stt_w2v2
-import sounddevice as sd
 
-    # Load weather api key
-load_dotenv('api_keys.env')
-api_key = os.getenv("OPENWEATHERMAP_KEY")
-
-    # Load model of speech to text
+# Load model of speech to text
 model, processor = stt_w2v2.load_model()
 
-    # Deploying FastAPI
+# Deploying FastAPI
 app = FastAPI()
 
 class Audio(BaseModel):
@@ -23,11 +17,28 @@ class Audio(BaseModel):
 @app.get('/get-response-from-audio/')
 async def get_response_from_audio(audio_path: Audio):
     
-    audio_array = stt_w2v2.audio_to_array(audio_path.audio_path)
-
-    predicted_sentence = stt_w2v2.inference_model(model, processor, audio_array)
+    if os.path.exists('audios_foxie/'):
+        path = 'audios_foxie/'
+        print(path)
+    else:
+        print('else')
+        path = '../bot/audios_telegram/'
+        print(path)
     
+    # Getting audio name
+    audio_name = audio_path.audio_path
+    
+    # Getting audio full path
+    audio_path = os.path.join(path, audio_name)
+
+    # Audio to array conversion
+    audio_array = stt_w2v2.audio_to_array(audio_path)
+
+    # Prediction
+    predicted_sentence = stt_w2v2.inference_model(model, processor, audio_array)
     output = jsonable_encoder({'Prediction':predicted_sentence[0]})
+
+
     """
     ### load audio
     # print(audio_path)
@@ -52,7 +63,6 @@ async def get_response_from_audio(audio_path: Audio):
 if __name__ == '__main__':
     import os
     import uvicorn
-    # os.system('uvicorn voice_assistant/voice_assistant_api:app --reload')
     uvicorn.run("voice_assistant_api:app", host="127.0.0.1", port=8000, log_level="info")
 
     # url = 'http://127.0.0.1:8000'
